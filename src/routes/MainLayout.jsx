1506c0f9228b4b6d40e08356c5fb21e9662b9bb9
martasavenok <magnolia.sav@gmail.com>
2025-02-23 23:24:10 +0300

    update site page"

--- src/routes/MainLayout.jsx
+++ src/routes/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from "@components/Site/Header.jsx";
+import AppInfoBlock from "@components/Site/AppInfoBlock.jsx";
 
 const MainLayout = () => {
     return (
         <>
             <Header/>
+            <AppInfoBlock/>
         </>
     );
 };
