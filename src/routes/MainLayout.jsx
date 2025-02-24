6771bffefec25b381119003ac883ddcc02c58ea0
martasavenok <magnolia.sav@gmail.com>
2025-02-24 12:03:26 +0300

    create testimonial swiper

--- src/routes/MainLayout.jsx
+++ src/routes/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "@components/Site/Header.jsx";
 import AppInfoBlock from "@components/Site/AppInfoBlock.jsx";
+import ResultsBlock from "@components/Site/ResultsBlock.jsx";
 
 const MainLayout = () => {
     return (
         <>
             <Header/>
             <AppInfoBlock/>
+            <ResultsBlock/>
         </>
     );
 };
