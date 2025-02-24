df047e9561f4733be97f5574ea9a1095f0a1242e
martasavenok <magnolia.sav@gmail.com>
2025-02-24 23:28:14 +0300

    update main page

--- src/routes/MainLayout.jsx
+++ src/routes/MainLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from "@components/Site/Header.jsx";
 import AppInfoBlock from "@components/Site/AppInfoBlock.jsx";
 import ResultsBlock from "@components/Site/ResultsBlock.jsx";
+import CoachBlock from "@components/Site/CoachBlock.jsx";
 
 const MainLayout = () => {
     return (
@@ -9,6 +10,7 @@ const MainLayout = () => {
             <Header/>
             <AppInfoBlock/>
             <ResultsBlock/>
+            <CoachBlock/>
         </>
     );
 };
