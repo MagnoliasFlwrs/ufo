84b4eedbf50969b2172e7212c7af213ba6ee1099
Yahor Yankavets <mr.yankovec@gmail.com>
2025-06-20 10:41:05 +0300

    Update weightUtils.js

--- src/utils/weightUtils.js
+++ src/utils/weightUtils.js
@@ -8,9 +8,17 @@ export const getDisplayWeight = (weightInKg, measurementSystem) => {
 };
 
 export const calculateGoalDate = (currentWeight, idealWeight) => {
-  const lostWeightPerWeek = currentWeight >= 90 ? 1 : 0.7;
-  const SECONDS_IN_WEEK = 7 * 24 * 60 * 60;
+  let lostWeightPerWeek;
 
+  if (currentWeight >= 90) {
+    lostWeightPerWeek = 1; // ≥90 кг → 1 кг/неделю
+  } else if (currentWeight > 65) {
+    lostWeightPerWeek = 0.7; // 65 < вес < 90 → 0.7 кг/неделю
+  } else {
+    lostWeightPerWeek = 0.5; // ≤65 кг → 0.5 кг/неделю
+  }
+
+  const SECONDS_IN_WEEK = 7 * 24 * 60 * 60;
   const weightDifference = Math.abs(currentWeight - idealWeight);
   const weeksNeeded = weightDifference === 0 ? 0 : weightDifference / lostWeightPerWeek;
 
